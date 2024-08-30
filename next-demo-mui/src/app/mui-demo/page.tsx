9c6fc9c0aad9020af9e9da7796a2fa6be3f78cac
Joseph Hart <joseph.hart@improving.com>
2024-08-29 21:21:48 -0500

    mui demo setup

--- next-demo-mui/src/app/mui-demo/page.tsx
+++ next-demo-mui/src/app/mui-demo/page.tsx
@@ -1,7 +1,7 @@
+import MuiDemoClientPage from "./_page.client";
+
 export default async function MuiDemoPage() {
   return (
-    <div>
-      <h1>MUI Demo Page</h1>
-    </div>
+    <MuiDemoClientPage title="MUI Demo Page" />
   );
 }
---
+++ next-demo-mui/src/app/theme-client.ts
@@ -0,0 +1,14 @@
+'use client';
+
+import { Inter } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+export const inter = Inter({ subsets: ["latin"] });
+
+const theme = createTheme({
+  typography: {
+    fontFamily: inter.style.fontFamily,
+  },
+});
+
+export default theme;
